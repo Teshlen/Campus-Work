576fe9ce2549191881d48406f940fbf8ae30a7e6
Teshlen <tnadesan@gmail.com>
2014-10-10 14:30:45 +0200

    cos330prac5

---
+++ cos330prac5/src/cos330prac5/Cos330prac5.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cos330prac5;
+
+/**
+ *
+ * @author Teshlen
+ */
+public class Cos330prac5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String message = "This is a test message to encrypt and decrypt";
+        String passPhrase = "Ethics";
+        
+        System.out.println("Message: ");
+        System.out.println(message);
+        System.out.println("");
+        System.out.println("Pass Phrase:");
+        System.out.println(passPhrase);
+        System.out.println("");
+        System.out.println("Encoded:");
+        
+        message = message.replaceAll("\\s+","");
+        message = message.toLowerCase();        
+        passPhrase = passPhrase.toLowerCase();        
+        cypher x = new cypher(message, passPhrase);
+        x.createTableau();
+        String encodedMessage = x.encode();
+        
+//        if(encodedMessage.matches("SGFXSOCIYXEDQZTTYPSNYLWWEOGTHURSACCHS"))
+//        {
+//            System.out.println("it works :)!!!!!!!");
+//        }
+        
+       // x.printTable();
+    }
+    
+}
---
+++ cos330prac5/src/cos330prac5/cypher.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cos330prac5;
+
+/**
+ *
+ * @author Teshlen
+ */
+public class cypher {
+
+    private String origionalText = "YTIRUCESZXWVQPONMLKJHGFDBA";
+    private String[][] vigenereTableau;
+    private String message, passphrase;
+    private String encodedMessage;
+
+    public cypher(String mes, String pas) {
+        vigenereTableau = new String[26][26];
+        message = mes;
+        passphrase = pas;
+        
+        encodedMessage = "";
+        
+         for (int i = 0; i < origionalText.length(); i++) {
+            for (int j = 0; j < origionalText.length() ; j++) {
+                vigenereTableau[i][j] = "@";
+
+            }
+  
+        }
+    }
+
+    public void createTableau() {
+        
+        
+        
+        for (int i = 0; i < origionalText.length(); i++) {
+
+            boolean flag = false;
+            int count = i;
+            for (int j = 0;j < origionalText.length() - i; j++) {
+               
+                vigenereTableau[i][j] = origionalText.charAt(count++) + "";
+            }
+            
+            int c = 0;
+            for (int j = origionalText.length() -i ; j < origionalText.length(); j++) {
+                
+                vigenereTableau[i][j] = origionalText.charAt(c++) + "";
+            }
+        }
+
+    }
+
+    public void printTable() {
+        for (int i = 0; i < origionalText.length(); i++) {
+            for (int j = 0; j < origionalText.length(); j++) {
+                System.out.print(vigenereTableau[i][j] + " ");
+
+            }
+            System.out.println("");
+        }
+
+    }
+    
+    public String encode(){
+    
+        String longPassPhrase = "";
+        origionalText = origionalText.toLowerCase();
+        
+        int numberOfPasses = (message.length() / passphrase.length()) +10;
+        
+        for (int i = 0; i < numberOfPasses; i++) {
+            longPassPhrase += passphrase;
+        }
+        
+        char letterOfMessage;
+        char letterOfPassphrase;
+        int indexOfLetterOfMessage;
+        int indexOfLetterOfPassphrase;
+        for (int i = 0; i < message.length(); i++) {
+            
+            letterOfMessage = message.charAt(i) ;
+            letterOfPassphrase = longPassPhrase.charAt(i);
+            
+            indexOfLetterOfMessage = origionalText.indexOf(letterOfMessage);
+//            System.out.println("mes" + indexOfLetterOfMessage);
+            indexOfLetterOfPassphrase = origionalText.indexOf(letterOfPassphrase) ;
+            
+            encodedMessage += vigenereTableau[indexOfLetterOfMessage][indexOfLetterOfPassphrase];
+            
+                        
+        }
+        System.out.println(encodedMessage);
+        return encodedMessage;
+        
+    }
+    
+    
+
+}
